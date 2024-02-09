e4b983a68861eb2dd8ca12c1a5a09f27ac00ee2c
Manuel <info@tagirijus.de>
2024-02-09 21:35:53 +0100

    Added ID for sidebar button

--- Template/task/task_sidebar_after.php
+++ Template/task/task_sidebar_after.php
@@ -1,3 +1,17 @@
+<?php
+
+$html = '<i class="fa fa-trash fa-fw js-modal-small" aria-hidden="true"></i>' . t('Duplicate (instant)');
+$href = $this->helper->url->href(
+    'DuplicateModController',
+    'duplicateInstant',
+    ['plugin' => 'DuplicateMod', 'task_id' => $task['id']]
+);
+$a_element = '<a href="' . $href . '" class="js-modal-small" id="duplicateModDuplicateInstant" data-addUrl="' . $href . '">' . $html . '</a>';
+
+?>
+
+<?php if ($task['is_active'] == 1): ?>
 <li>
-    <?= $this->modal->small('files-o', t('Duplicate (instant)'), 'DuplicateModController', 'duplicateInstant', array('task_id' => $task['id'], 'plugin' => 'DuplicateMod')) ?>
+    <?php echo $a_element; ?>
 </li>
+<?php endif ?>
